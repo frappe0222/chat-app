157a00ba9ffa38edb274aae5a1ae440b70fe8d1d
frappe <frappeccino0222@gmail.com>
2020-07-27 15:41:31 +0900

    nokori

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,43 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+<<<<<<< HEAD
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+=======
+    @messages = @room.messages.includes(:user)
+>>>>>>> 29b34bbd07255a58ae195cf0e2bfd2fa51327a48
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+<<<<<<< HEAD
+  resources :rooms, only: [:new, :create] do
+   resources :messages, only: [:index, :create]
+
+  end
+=======
+  resources :rooms, only: [:new, :create, :destroy] do
+    resources :messages, only: [:index, :create]
+ end
+>>>>>>> 29b34bbd07255a58ae195cf0e2bfd2fa51327a48
 end
